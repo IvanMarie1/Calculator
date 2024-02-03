45ea1c931ad12b7efce702440fe26fcf74339f35
Ivan <89919994+IvanMarie1@users.noreply.github.com>
2024-02-03 20:27:39 +0100

    Update calc.py
    add more documentation and correct some texts

--- code/calc.py
+++ code/calc.py
@@ -1,12 +1,24 @@
 import re
 
 
-def operation(a: float, b: float, operator: str) -> float | str:
-    """Return the result of a basic operation
+def operation(a: float, b: float, operator: str) -> float | int | str:
+    """Calculates a simple operation between two floats
+
+    Parameters
+    ---------
+    a : float
+        left term of the operation
+    b : float
+        right term of the operation
+    operator : str
+        symbol of the  mathematical operator (only + - * / supported)
     
-    Only [ + - * / ] supported
-    
-    Return "ZeroDivisionError" if you divide by zero"""
+    Returns
+    -------
+    float or int
+        result of the operation (simplified)
+    str
+      "ZeroDivisionError" if you divide by zero"""
     
     if operator == "+":
         result = a + b
@@ -19,11 +31,25 @@ def operation(a: float, b: float, operator: str) -> float | str:
             result = a / b
         except ZeroDivisionError:
             return "Zero Division Error"
-    return result
+    return simplify(result)
 
 
-def closed_parenthesis(text, i_start) -> int:
-    """Return the index at which the parenthesis is closed, if not return a text error"""
+def closed_parenthesis(text: str, i_start: int) -> int | str:
+    """Find the index of the closing parenthesis in a string
+
+    Parameters
+    ----------
+    text : str
+        Text containing parentheses
+    i_start : int
+        Index of the opening parenthesis
+    
+    Returns
+    -------
+    int
+        index at which the parenthesis is closed
+    str
+        "( not closed" if the parenthesis is not closed"""
     i_end = i_start + 1
 
     while i_end < len(text):
@@ -37,8 +63,21 @@ def closed_parenthesis(text, i_start) -> int:
 
 def simplify(number: str) -> int | float | str:
     """Convert a string number into int or float according to its value
+
+    Parameters
+    ----------
+    number : str
+        Number to convert type
     
-    If it is not a number, return a text error"""
+    Returns
+    -------
+    int
+        number as an integer if it is
+    float
+        number as a float if it is decimal
+    str
+        "Invalid Syntax" if it is neither
+    """
     try:
         result = float(number)
         if result.is_integer():
@@ -48,9 +87,17 @@ def simplify(number: str) -> int | float | str:
         return "Invalid Syntax"
 
 def split_calculation(calc: str) -> list[str]:
-    """Split a calculation in different element : parenthesis until it's closed, numbers, operators
+    """Split a calculation in different element : parentheses, numbers, operators
+
+    Parameters
+    ---------
+    calc : str
+        Calculation line to split
     
-    Return a list of the calcultion's terms"""
+    Returns
+    -------
+    list[str]
+        list of the calcultion terms"""
     terms = []
     i_start = 0
     i_end = 0
@@ -73,30 +120,45 @@ def split_calculation(calc: str) -> list[str]:
 
 
 def my_split(text, i_start, i_end) -> list[str]:
-    """Split a calculation with re module
+    """Split numbers and operators between two indexes with re module
 
-    Return a list of the splited numbers and operators"""
+    Parameters
+    ----------
+    text : str
+        Text to split up
+    i_start : int
+        Index of the starting position
+    i_end : int
+        Index of the ending position
+
+    Returns
+    -------
+    list[str] 
+        list of the splited numbers and operators"""
     my_pattern = r'(\+|(?<![/*+])-|\/|\*)'
     my_first_pattern = r'(\+|(?<![/*+])(?!^-)-|\/|\*)'
 
     if i_start == 0:
-        return ' '.join(re.split(my_first_pattern, text[:i_end])).split() # handle first neg number
+        return ' '.join(re.split(my_first_pattern, text[:i_end])).split() # first neg number included
     return ' '.join(re.split(my_pattern, text[i_start:i_end])).split()
 
 
 
 
-def calculate(calc: str) -> float | str:
-    """Return the result of a calculation string
-
-    Only support [ + - * / ]
-
-    If an error occurs, it returns an error message
-
-    ---
-    TODO 
-    - add other operators like exponent, sqrt, etc.
-    - "intelligent" caluculations like (2+3)(2-3) without operator
+def calculate(calc: str) -> float | int | str:
+    """Give the result of a calculation line
+    
+    Parameters
+    ----------
+    calc : str
+        calculation line to calculate
+    
+    Returns
+    -------
+    int | float
+        result of the calculation
+    str
+        error message if ones occurs
     """
 
     # only supported characters
@@ -105,7 +167,7 @@ def calculate(calc: str) -> float | str:
         if char not in supported_char:
             return "Unknown character"
     
-    # if it is englobed by parentheses, remove them
+    # if it is surronded by parentheses, remove them
     if calc.startswith('('):
         if closed_parenthesis(calc, 0) == len(calc) - 1:
             calc = calc[1:-1]
@@ -132,6 +194,7 @@ def calculate(calc: str) -> float | str:
     operator = terms[i_operator]
     left_term = calculate(''.join(terms[:i_operator]))
     right_term = calculate(''.join(terms[i_operator+1:]))
+    
     if isinstance(left_term, str):
         return left_term
     elif isinstance(right_term, str):
