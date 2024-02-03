48aee485a30a43e96520a019eec94985cd53224c
Ivan <89919994+IvanMarie1@users.noreply.github.com>
2024-02-03 20:30:19 +0100

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,7 +1,8 @@
-from graphic import create_calc
+from graphic import Calculator
 
 
 if __name__ == "__main__":
-    my_calc, text = create_calc()
 
-    my_calc.mainloop()
+    my_calc1 = Calculator('My calculator :)', 333)
+    my_calc1.setup()
+    my_calc1.run()
